db6369360ea533e68686c346958e85508ffd3502
M4th3usao <88057724+M4th3usao@users.noreply.github.com>
2022-09-23 22:17:57 -0300

    4 files modified

--- src/app/tabelaComponent/armotizacao/armotizacao.component.ts
+++ src/app/tabelaComponent/armotizacao/armotizacao.component.ts
@@ -6,22 +6,47 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./armotizacao.component.css'],
 })
 export class ArmotizacaoComponent implements OnInit {
-  @Input() PMT: string;
-  @Input() Pv: string;
-  @Input() i: string;
-  @Input() n: string;
+  c: number = 100;
+  i: number = 0.2;
+  t: number = 8;
+
   constructor() {}
 
   ngOnInit() {}
 
-  getPrice() {
-    return (
-      Number(this.Pv) *
-      (((1 + Number(this.i)) ** Number(this.n) * Number(this.i)) /
-        (1 + Number(this.i) ** Number(this.n) - 1))
-    );
+  getSplit() {
+    return (this.c * this.i) / (1 - 1 / Math.pow(1 + this.i, this.t));
+  }
+
+  getPeriodo() {
+    var periods = [];
+    for (var i = 1; i <= this.t; i++) {
+      periods.push(i);
+    }
+    return periods;
+  }
+
+  getInteresse(n: number) {
+    var value = this.c;
+    for (var i = 0; i < n; i++) {
+      value = value + value * this.i - this.getSplit();
+    }
+    return value * this.i;
+  }
+
+  getAmortizacao(n: number) {
+    var value = this.c;
+    for (var i = 0; i < n; i++) {
+      value = value + value * this.i - this.getSplit();
+    }
+    return this.getSplit() - value * this.i;
+  }
+
+  getBalanceamento(n: number) {
+    var value = this.c;
+    for (var i = 0; i < n; i++) {
+      value = value + value * this.i - this.getSplit();
+    }
+    return value;
   }
 }
-
-// Pv *[(1+i)n * i] / [(1+i)n – 1]
-// return Number(this.c) * (1 * Number(this.i)) ** Number(this.t);
